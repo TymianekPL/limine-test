779b157827a84335e4fab2cf5d0a10373ba13b18
TymianekPL <wilku.tymon@gmail.com>
2022-12-08 16:57:10 +0100

    Remove dead code

--- kernel/gdt.h
+++ kernel/gdt.h
@@ -34,32 +34,4 @@ extern GDT DefaultGDT;
 
 extern void LoadGDT(GDTDescriptor *gdtDescriptor);
 
-// typedef struct SegmentDescriptor_t
-// {
-//     uint16_t limit_lo;
-//     uint16_t base_lo;
-//     uint8_t base_hi;
-//     uint8_t type;
-//     uint8_t flags_limit_hi;
-//     uint8_t base_vhi;
-// } __attribute__((packed)) SegmentDescriptor;
-
-// struct SegmentDescriptor *GetSegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
-// void SetSegmentDescriptor(SegmentDescriptor *segment, uint32_t base, uint32_t limit, uint8_t type);
-
-// uint32_t GetBaseFromSegment(SegmentDescriptor *segment);
-// uint32_t GetLimitFromSegment(SegmentDescriptor *segment);
-// uint8_t GetTypeFromSegment(SegmentDescriptor *segment);
-
-// typedef struct GlobalDescriptorTable_t
-// {
-//     SegmentDescriptor nullSegmentSelector;
-//     SegmentDescriptor kernelCode64SegmentSelector;
-//     SegmentDescriptor kernelDataSelector;
-//     SegmentDescriptor userDataSegmentSelector;
-//     SegmentDescriptor userCode64SegmentSelector;
-// } __attribute__((packed)) GlobalDescriptorTable;
-
-// void InitializeGDT(GlobalDescriptorTable *gdt);
-
 #endif
